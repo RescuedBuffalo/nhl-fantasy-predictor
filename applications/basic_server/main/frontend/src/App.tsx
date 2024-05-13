c282caa546d687adafbfbd6ff6d6554af8a2ef11
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-05-12 18:25:20 -0700

    added error handling and loading to fe

--- applications/basic_server/main/frontend/src/App.tsx
+++ applications/basic_server/main/frontend/src/App.tsx
@@ -4,21 +4,35 @@ import DataTable from './DataTable';
 
 const App = () => {
   const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const fetchData = async () => {
     try {
+      setLoading(true);
       const response = await fetch('http://localhost:8000/data');
       const jsonData = await response.json();
       setData(jsonData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
+      setError('');
+    } catch (err) {
+      setError('Failed to fetch data');
       setData([]);
+    } finally {
+      setLoading(false);
     };
 
   return (
     <div className="App">
-      <button onClick={fetchData} style={{ marginTop: '20px', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
-        Fetch Data
+      <button onClick={fetchData} 
+          disabled={loading}
+          style={{ 
+            marginTop: '20px', 
+            position: 'absolute', 
+            left: '50%', 
+            transform: 'translateX(-50%)' 
+            }}
+      >
+        {loading ? 'Loading...' : 'Fetch Data'}
       </button>
       <DataTable data={data}/>
     </div>
