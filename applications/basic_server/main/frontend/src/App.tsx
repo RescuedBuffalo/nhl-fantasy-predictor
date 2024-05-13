5bfd773687b4ffdcd944bcf29868a4cb23fcf4a3
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-05-12 18:21:01 -0700

    update fe app

--- applications/basic_server/main/frontend/src/App.tsx
+++ applications/basic_server/main/frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import DataTable from './DataTable';
 
 const App = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/data');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setData([]);
+    };
+
   return (
     <div className="App">
-      <button style={{ marginTop: '20px', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
+      <button onClick={fetchData} style={{ marginTop: '20px', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
         Fetch Data
       </button>
-      <DataTable />
+      <DataTable data={data}/>
     </div>
   );
 }
