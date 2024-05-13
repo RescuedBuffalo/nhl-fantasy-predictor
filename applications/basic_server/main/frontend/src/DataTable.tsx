06f61e636571cb57084be494ddcc37b1f2de083d
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-05-12 18:20:56 -0700

    update data table

--- applications/basic_server/main/frontend/src/DataTable.tsx
+++ applications/basic_server/main/frontend/src/DataTable.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
-const DataTable = () => {
+const DataTable = ( {data} ) => {
   return (
     <table style={{ width: '100%', marginTop: '60px' }}>
       <thead>
         <tr>
           <th>ID</th>
           <th>Name</th>
-          <th>Details</th>
+          <th>Position</th>
+          <th>Team</th>
+          <th>Goals</th>
         </tr>
       </thead>
       <tbody>
+        {data.map(item => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.position}</td>
+            <td>{item.team}</td>
+            <td>{item.goals}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
